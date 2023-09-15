668cae97c327e3c53d2232068b6aa3df08fe33d2
tobii-dev <77458451+tobii-dev@users.noreply.github.com>
2023-09-15 16:36:26 +0200

    add: clean version (without logging)

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
 use blur_plugins_core::{BlurAPI, BlurEvent, BlurPlugin};
-use log::LevelFilter;
-use simplelog::{
-	ColorChoice, CombinedLogger, Config, ConfigBuilder, TermLogger, TerminalMode, WriteLogger,
-};
 
 #[repr(C)]
 struct MyDummyPlugin {}
@@ -12,45 +8,13 @@ impl BlurPlugin for MyDummyPlugin {
 		"MyDummyPlugin"
 	}
 
-	fn on_event(&self, event: &BlurEvent) {
-		match &event {
-			BlurEvent::NoEvent => {}
-			BlurEvent::Login(_u) => {}
-			BlurEvent::Screen(_u) => {}
-		}
-		log::info!("Event: {event:?} from plugin: {}", self.name());
-	}
+	fn on_event(&self, _event: &BlurEvent) {}
 
-	fn free(&self) {
-		log::info!("Unloading plugin: {}", self.name());
-	}
+	fn free(&self) {}
 }
 
 #[no_mangle]
 fn plugin_init(_api: &mut dyn BlurAPI) -> Box<dyn BlurPlugin> {
 	let plugin = MyDummyPlugin {};
-	let cfg = ConfigBuilder::new()
-		.set_time_offset_to_local()
-		.unwrap()
-		.build();
-	let log_path = std::format!(".\\amax\\log\\{}.log", plugin.name());
-
-	CombinedLogger::init(vec![
-		TermLogger::new(
-			LevelFilter::Trace,
-			cfg,
-			TerminalMode::Mixed,
-			ColorChoice::Auto,
-		),
-		WriteLogger::new(
-			LevelFilter::Trace,
-			Config::default(),
-			std::fs::File::create(&log_path)
-				.expect(&std::format!("Couldn't create log file: {log_path}")),
-		),
-	])
-	.unwrap();
-	log::info!("Init plugin: {}", plugin.name());
-
 	Box::new(plugin)
 }
